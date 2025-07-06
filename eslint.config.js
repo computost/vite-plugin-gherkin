fec1cce4f492ccfac06b1c7604b8ca19208bd398
Melody Universe <melody@computost.com>
2025-07-06 13:03:47 -0700

    ts-check eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
+// @ts-check
+
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
+export default tseslint.config([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: [js.configs.recommended],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
@@ -20,12 +21,12 @@ export default defineConfig([
     files: ["**/*.json"],
     plugins: { json },
     language: "json/json",
-    extends: ["json/recommended"],
+    extends: [json.configs.recommended],
   },
   {
     files: ["**/*.md"],
     plugins: { markdown },
     language: "markdown/gfm",
-    extends: ["markdown/recommended"],
+    extends: [markdown.configs.recommended],
   },
 ]);
